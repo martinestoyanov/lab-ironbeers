adfdfda145e3aad6a2e7ea1d9e88a8d15b9ca305
Martin Stoyanov <martinestoyanov@gmail.com>
2021-03-09 21:46:38 -0500

    Basic linking established

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
-
+const app = express();
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
-const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
@@ -20,6 +18,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  res.render('beers');
+});
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
